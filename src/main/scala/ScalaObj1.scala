b0dc34ce3cc502e617d1f479d19d78c5dc30ff15
vivek <viveks4grps@gmail.com>
2014-08-16 15:44:34 +0530

    Changed the Author name

--- src/main/scala/ScalaObj1.scala
+++ src/main/scala/ScalaObj1.scala
@@ -5,5 +5,6 @@ object ScalaObj1 {
   def main(args: Array[String]) {
     println("Welcome to the scala world")
     println("With Git it works!!!")
+    println("ScalaObj1.scala revisited")
   }
 }
